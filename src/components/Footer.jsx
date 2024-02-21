3e75c62de781e269c0ad7d8b154fbd713dbc41ba
Gabe <96975640+Gabe9T@users.noreply.github.com>
2024-02-20 16:24:30 -0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <>
       <div id='flexParent'>
         <div id='flexChild'>
-          <a id="footerLink" href="https://github.com/kimmykokonut/sound-check">
+          <a id="footerLink" href="https://github.com/Gabe9T/Sound-Check">
             Designed and built by Team Awesome
           </a>
         </div>
